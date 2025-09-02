dedb179e2079214a3c49bbf32fb167dcfe982dc2
David Lynch <98290029+coillteoir@users.noreply.github.com>
2025-09-02 11:28:32 +0100

    feat: adding check to ensure valid cue schema (#21)
    * feat: adding check to ensure valid cue schema
    
    * fix: types
    
    * using stat to detect cue.mod directory
    
    * fix: go mod
    
    * fix: cuestomize image
    
    * go fmt

--- internal/pkg/cuestomize/oci/fetch.go
+++ internal/pkg/cuestomize/oci/fetch.go
@@ -3,10 +3,11 @@ package oci
 import (
 	"context"
 	"fmt"
-
 	"github.com/Workday/cuestomize/api"
 	"github.com/Workday/cuestomize/internal/pkg/fetcher"
 	"github.com/rs/zerolog/log"
+	"os"
+	"path/filepath"
 	kyaml "sigs.k8s.io/kustomize/kyaml/yaml"
 )
 
@@ -34,5 +35,10 @@ func FetchFromRegistry(ctx context.Context, config *api.KRMInput, items []*kyaml
 	); err != nil {
 		return fmt.Errorf("failed to fetch from OCI registry: %w", err)
 	}
+	_, err = os.Stat(filepath.Join(workingDir, "cue.mod"))
+	if err != nil {
+		log.Warn().Err(err).Msg("cue.mod directory not found. Please ensure cue.mod exists in artifact")
+	}
+
 	return nil
 }
