449e4c1cef5238a92064fd90edd3844f5537d6a6
Lorenzo Felletti <60483783+lorenzofelletti@users.noreply.github.com>
2025-09-09 14:48:20 +0100

    ci: add dry-run as an option to Renovate function and update workflow permissions (#23)

--- .dagger/deps_update.go
+++ .dagger/deps_update.go
@@ -11,7 +11,14 @@ func (m *Cuestomize) Renovate(
 	githubToken *dagger.Secret,
 	// +default="Workday/cuestomize"
 	repo string,
+	// +default=false
+	dryRun bool,
 ) *dagger.Container {
+	args := []string{}
+	if dryRun {
+		args = append(args, "--dry-run")
+	}
+
 	cacheHack := time.Now() // avoid dagger to cache the container
 	return dag.Container().From("renovate/renovate:latest").
 		WithSecretVariable("RENOVATE_TOKEN", githubToken).
@@ -21,5 +28,5 @@ func (m *Cuestomize) Renovate(
 		WithEnvVariable("RENOVATE_GIT_AUTHOR", "Renovate Bot <bot@renovateapp.com>").
 		WithEnvVariable("RENOVATE_REPOSITORIES", repo).
 		WithEnvVariable("CACHE_HACK", cacheHack.String()).
-		WithExec([]string{"--dry-run"}, dagger.ContainerWithExecOpts{UseEntrypoint: true})
+		WithExec(args, dagger.ContainerWithExecOpts{UseEntrypoint: true})
 }
